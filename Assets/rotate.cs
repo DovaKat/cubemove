01064bc21152ffbc62c2cacb09c0b4c63e0815b1
dovakat <katswow@hotmail.co.uk>
2015-02-10 15:55:46 +0000

    initial commit

---
+++ Assets/rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	public float speed = 60f;
+	float angle = 0f;
+	
+
+	
+	// Update is called once per frame
+	void Update () {
+		angle = angle + Time.deltaTime * speed;
+		transform.rotation = Quaternion.Euler (0f, angle, 0f);
+	
+	}
+}
